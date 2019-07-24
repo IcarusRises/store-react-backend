c175940e805c2a1bb5276aa1eb59b7028679a9c2
Billie Tran <0billtran@gmail.com>
2019-07-24 02:30:17 -0700

    Connected Backend to Database and aquired and using routes

--- server.js
+++ server.js
@@ -1,13 +1,35 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+const router = express.Router();
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const PORT = 3001;
+
+require('dotenv').config();
 
 app.use(cors());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
-app.listen(PORT, () => {
+
+//MONGODB DATABASE
+const dbRoute = process.env.DB;
+
+//CONNECTING BACKEND TO DATABASE
+mongoose.connect(dbRoute, {useNewUrlParser:true, useCreateIndex: true}).then(()=>{
+    console.log('Connected to Database');
+}).catch(err => {
+    console.log('Could not connect to Database', err)
+});
+
+//REQUIRING ROUTES
+const laptopRoutes = require('../Backend/routes/laptops');
+
+//USING ROUTES
+app.use('/laptops', laptopRoutes);
+
+//APP LISTENER
+app.listen(process.env.PORT || PORT, () => {
     console.log('Server ' + PORT + ' is up and running Captain')
 })
