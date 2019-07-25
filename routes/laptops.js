37ddd921f6d3c493cc3785b750830d0302299e25
Billie Tran <0billtran@gmail.com>
2019-07-25 03:22:27 -0700

    Finished adding the rest of the routes

--- routes/laptops.js
+++ routes/laptops.js
@@ -1,20 +1,50 @@
 const router = require('express').Router();
 let   Laptop = require('../models/laptop.model');
 
+//LAPTOPS
 router.route('/').get((req, res) => {
     Laptop.find()
         .then(laptops => res.json(laptops))
         .catch(err => res.status(400).json('ERROR ' + err));
 });
 
+//LAPTOP
+router.route('/:id').get((req, res) => {
+    Laptop.findById(req.params.id)
+        .then(laptop => res.json(laptop))
+        .catch(err => res.status(400).json('ERROR: ' + err));
+});
+
+//ADD LAPTOP
 router.route('/add').post((req, res) => {
     const brand = req.body.brand;
 
     const newLaptop = new Laptop({brand});
 
     newLaptop.save()
-        .then(() => res.json('Successfully Added ' + newLaptop.brand[0].toUpperCase() + newLaptop.brand.substring(1)))
+        .then(() => res.json('Successfully Added ' + newLaptop.brand))
         .catch(err => res.status(400).json('ERROR ' + err));
 });
+
+//UPDATE LAPTOP
+router.route('/update/:id').post((req, res) => {
+    Laptop.findByIdAndUpdate(req.params.id)
+        .then(laptop => {
+            laptop.brand = req.body.brand;
+
+            laptop.save()
+            .then(() => res.json(laptop.brand + ' has been updated!'))
+            .catch(err => res.status(400).json('ERROR: ' + err))
+        })
+        .catch(err => res.status(400).json('ERROR: ' + err));
+});
+
+//DELETE LAPTOP
+router.route('/:id').delete((req, res) => {
+
+    Laptop.findByIdAndDelete(req.params.id)
+        .then(() => res.json(req.body.brand + ' Deleted'))
+        .catch(err => res.status(400).json('ERROR: ' + err))
+})
 
 module.exports = router;
