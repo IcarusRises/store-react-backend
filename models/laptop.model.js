669aec4078d136fdd3ddafd3509ab769c6cf297a
Billie Tran <0billtran@gmail.com>
2019-08-06 01:46:17 -0700

    Added more to model schema

--- models/laptop.model.js
+++ models/laptop.model.js
@@ -2,6 +2,13 @@ const mongoose = require("mongoose");
 
 const laptopSchema = new mongoose.Schema({
     brand: String,
+    name: String,
+    image: String,
+    image2: String,
+    image3: String,
+    original: Number,
+    savings: Number,
+    price: Number
 });
 
 module.exports = mongoose.model("Laptop", laptopSchema); 
